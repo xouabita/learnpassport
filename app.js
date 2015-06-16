88fcbf5a36865b2b2ae9578a981ad6178430792b
xouabita <xouabita@gmail.com>
2015-06-16 12:13:32 +0800

    add stuff to app.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 // dependencies
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
-var routes = require('./routes');
-var path = require('path');
-var mongoose = require('mongoose');
+var path          = require('path')
+  , express       = require('express')
+  , http          = require('http')
+  , mongoose      = require('mongoose')
+  , passport      = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 // global config
 var app = express();
 app.set('port', process.env.PORT || 1337);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.set('view options'. {layout: false});
 app.use(express.favicon());
+app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('my secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,17 +32,17 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
-// mongo config
-var MONGOLAB_URI= "add_your_mongolab_uri_here"
-var mongo = process.env.MONGOLAB_URI || 'mongodb://localhost/node-bootstrap3-template'
-mongoose.connect(mongo);
+// Passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUSer());
 
-// mongo model
-// var Model_Name = require('add_your_models_here');
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
 
 // routes
-app.get('/', routes.index);
-app.get('/ping', routes.ping);
+require('./routes')(app);
 
 // run server
 app.listen(app.get('port'), function(){
